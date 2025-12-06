aeb7d9e7ed737a764d5021899b091e37311f1a7d
Paul Louth <p.louth@gmail.com>
2025-12-06 01:08:02 +0000

    Expand parser combinator `p4` to handle additional inputs and update parsing logic accordingly.

--- StackParsecPrototype/Program.cs
+++ StackParsecPrototype/Program.cs
@@ -6,16 +6,22 @@ Span<byte> stackMem = stackalloc byte[1024];
 //var p1 = pure("testing").Map(s => s.Length);
 //var p2 = pure("testing").Bind(s => pure(s.Length));
 //var p3 = take(3);
- var p4 = from x in take1
-          from y in take1
-          from z in take1
-          select (x, y, z);
+ var p4 = from x1 in take1
+          from y1 in take1
+          from z1 in take1
+          from x2 in take1
+          from y2 in take1
+          from z2 in take1
+          from x3 in take1
+          from y3 in take1
+          from z3 in take1
+          select (x1, y1, z1, x2, y2, z2, x3, y3, z3);
 
 /*var p5 = pure("testing")
             .Map(s => pure(s.Length))
             .Flatten();*/
 
-var r = p4.Parse("abc".AsSpan(), stackMem);
+var r = p4.Parse("abcabcabc".AsSpan(), stackMem);
 
 switch (r)
 {
