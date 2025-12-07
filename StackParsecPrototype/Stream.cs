c32caa1fdbf593ff2ca552576bc8cef8e0666e63
Paul Louth <p.louth@gmail.com>
2025-12-07 15:48:06 +0000

    Remove unused `StackParsecPrototype` module
    Deleted obsolete `StackParsecPrototype` module and all associated files, including `ArrayExtensions.cs`, `Bytes.cs`, `ConsAdd.cs`, `OpCode.cs`, `Parsec.CharModule.cs`, and `Parsec.cs`.

--- StackParsecPrototype/Stream.cs
+++
@@ -1,8 +0,0 @@
-namespace StackParsecPrototype;
-
-public interface Stream<S, T>
-    where S : Stream<S, T>, allows ref struct
-{
-    public abstract ReadOnlySpan<T> Tokens { get; }
-    public abstract int Count { get; }
-}
